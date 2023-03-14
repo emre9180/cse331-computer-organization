1a1cae72e22a66f623f24a41b9a015cf732f140d
Emre <79599681+emre9180@users.noreply.github.com>
2023-03-14 17:33:47 +0300

    Delete HW1/HW2 directory

--- HW1/HW2/cse331-hw2/adder_32bit.v
+++
@@ -1,9 +0,0 @@
-module adder_32bit(input [31:0] A, input [31:0] B, input Cin, output [31:0] S, output Cout);
-
-wire[3:0] firstCarry;
-
-adder_16bit adder1(A[15:0], B[15:0], Cin, S[15:0], firstCarry[0]);
-adder_16bit adder2(A[31:16], B[31:16], firstCarry[0], S[31:16], Cout);
-
-
-endmodule
